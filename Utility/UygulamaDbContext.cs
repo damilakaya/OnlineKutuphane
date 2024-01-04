d15553e35fb4048f6758b04817a86277e8a603b3
damilakaya <99400777+damilakaya@users.noreply.github.com>
2024-01-04 16:28:45 +0300

    Add files via upload

---
+++ Utility/UygulamaDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebUygulamaProje1.Entity;
+using WebUygulamaProje1.Models;
+
+// veri tabanında EF tablo oluşturması için ilgili model sınıfları buraya eklenmeli
+
+namespace WebUygulamaProje1.Utility
+{
+    public class UygulamaDbContext : IdentityDbContext
+
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) :  base(options) { }
+
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
+
+        public DbSet<Kitap> Kitaplar { get; set; }
+
+        public DbSet<Kiralama> Kiralamalar { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        
+
+      
+
+
+    }
+}
